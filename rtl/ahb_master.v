570608193eb75939da00e8eb1f34da5192f00d89
Aman Ahuja <48840330+amanahuja62@users.noreply.github.com>
2021-02-28 16:16:46 +0530

    Update ahb_master.v

--- rtl/ahb_master.v
+++ rtl/ahb_master.v
@@ -73,7 +73,9 @@ module ahb_master(
 			
         reg[3:0] beatCounter, countLimit; //keeps track of the number of beats in a burst
 		  reg[7:0] driveEnable;
-		  reg resetBC, incBC, sampleData, error,set3,set7,set15;
+		  reg[1:0] htrans;
+		  
+		  reg resetBC, incBC, sampleData, error,set3,set7,set15, pre_write;
 		  
 		  //beatCounter
 		  always@(posedge HCLK) begin
@@ -182,6 +184,7 @@ module ahb_master(
 			
 		  /// registers which store the current and next state
 		  reg[2:0] c_state, n_state;
+		 
 		  
 		 
 		
@@ -194,243 +197,287 @@ module ahb_master(
 			c_state<=n_state;
 		  end
 		  
+		  always@(posedge HCLK) begin
+			pre_write<=HWRITE;
+		  end
 		  
 		  
 		  always@(*) begin
 		  
-		  driveEnable='h00; n_state=s_idle; sampleData=0; incBC=0; error=0; resetBC=1;
-		  set3=0; set7=0; set15=0;
-			case(c_state)	
-					s_idle:begin
-								 driveEnable='hFF;
-								 nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
-								 
-					       end
-					s_fbtr: begin
-					          if(HREADY) begin
-										if(beatCounter==countLimit) begin
-												sampleData=1;
-												driveEnable='hfe;
-												nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
-												
-										end
-										
-										
-										else begin
-											driveEnable='h84;
-											sampleData=1;
-												if(iHTRANS==SEQ) begin
-														incBC=1;
-														n_state=s_fbtr;
-												end
-												
-												else begin
-													n_state=s_fbtr;
-												end
+			  driveEnable='h00; n_state=s_idle; incBC=0; error=0; resetBC=1;
+			  set3=0; set7=0; set15=0;		  
+			  if(pre_write=='b1)
+			  sampleData=0;
+			  else
+			  sampleData=1;
+		  
+				case(c_state)	
+						s_idle:begin
+									 if(HREADY) begin
+										 driveEnable='hFF;
+										 nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
+									 end
+									 else begin
+										 n_state=s_idle;
+										 driveEnable='h00;
+									 end
+									 
+								 end
+						s_fbtr: begin
+									 if(HREADY) begin
+											if(beatCounter==countLimit) begin
+													sampleData=1;
+													driveEnable='hfe;
+													nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
 													
+											end
 											
-										end
-								 
-								 
-								 
-								 end
-								 
-								 else begin
-										if(!HRESP) begin
-												driveEnable='h00;
-												n_state=s_fbtr;
+											
+											else begin
+												driveEnable='h84;
+												sampleData=1;
+													if(iHTRANS==SEQ) begin
+															incBC=1;
+															n_state=s_fbtr;
+													end
+													
+													else begin
+														n_state=s_fbtr;
+													end
+														
 												
-										end
-										
-										else begin
-												error=1;
-												n_state=s_idle;
-										end
-										
-								 end
-							  end
-							  
-							  
-					s_ubtw: begin
-								 if(HREADY) begin
-								   if(iHTRANS==IDLE || iHTRANS==NONSEQ)
-										driveEnable='hff;
-										
-									else
-										driveEnable='h85;
-								
-									   nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
-								 end
-								 
-								 else begin
-									if(HRESP) begin
-										error=1;
-										n_state=s_idle;
-									end
-									
-									else begin
-										driveEnable='h00;
-										n_state=s_ubtw;
-									end
-								 end
-							  end
-							  
-							  
-					s_fbtw: if(HREADY) begin
-									if(beatCounter==countLimit)begin
-									  driveEnable='hff;
-									  nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
-									  
-									end
-									
-									else begin
-										driveEnable='h85;
-										if(iHTRANS==SEQ) begin
-											incBC=1;
-											n_state=s_fbtw;
-									
-										end
+											end
+									 
+									 
+									 
+									 end
+									 
+									 else begin
+											if(!HRESP) begin
+											    if(HTRANS!=BUSY) begin
+													driveEnable='h00;
+													n_state=s_fbtr;
+											    end
+												 
+												 else begin
+													driveEnable='h84;
+													n_state=s_fbtr;
+												 end
+													
+											end
+											
+											else begin
+													error=1;
+													n_state=s_idle;
+											end
+											
+									 end
+								  end
+								  
+								  
+						s_ubtw: begin
+									 if(HREADY) begin
+										if(iHTRANS==IDLE || iHTRANS==NONSEQ)
+											driveEnable='hff;
+											
 										else
-										n_state=s_fbtw;									
+											driveEnable='h85;
 									
-									end
-					
-					
-							  end
-							  
-							  else begin
-									if(HRESP=='b0) begin
-										driveEnable='h00;
-										n_state=s_fbtw;
-									end
-									
-									else begin
-										error=1;
-										n_state=s_idle;
-									end
-							  end
-					
-					
-					
-					s_ubtr: begin
-									if(HREADY) begin
-										driveEnable='hfe;
-										sampleData=1;
-										nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
-									end
-									
-									else begin
-									   if(HRESP) begin
+											nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
+									 end
+									 
+									 else begin
+										if(HRESP) begin
 											error=1;
 											n_state=s_idle;
 										end
 										
 										else begin
-											driveEnable='h00;
-											n_state=s_ubtr;
+											if(HBURST==BUSY)
+												begin
+													driveEnable='hfe;
+													nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
+												end
+											else begin
+													driveEnable='h00;
+													n_state=s_ubtw;
+											end
+									   end
+									 end
+								  end
+								  
+								  
+						s_fbtw: if(HREADY) begin
+										if(beatCounter==countLimit)begin
+										  driveEnable='hff;
+										  nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
+										  
 										end
-									end
-							  end
-		   
-					
-					default: begin 
-									error=1;
-									n_state=s_idle;
-					         end
-			 endcase
-		  end
-		  
-		  
-		 
-   /////////task which determines the next state//////////////////
-		task nextStateDecider;
-			  input[1:0] htrans;
-			  input[2:0] hburst;
-			  input hwrite;
-			  output [2:0] n_state; 
-			  output  resetBC;
-			  output set3;
-			  output set7;
-			  output set15;
-			    begin
-				       n_state=s_idle;
-						 resetBC=1;
-						 set3=0;set7=0;set15=0;
-						 if(htrans==NONSEQ) begin		
-						      
-								if(hburst==INCR) 
-									if(hwrite=='b1)
-									n_state=s_ubtw;
-									else
-									n_state=s_ubtr;						
 										
-								 else begin 
-									resetBC=0;
-									case(hburst)
-										INCR4: set3=1;
-										INCR8: set7=1;
-										INCR16: set15=1;
-										WRAP4: set3=1;
-										WRAP8: set7=1;
-										WRAP16: set15=1;
-										default: set3=1;
-									endcase
-									if(hwrite=='b1)
-									  n_state=s_fbtw;
-									else
-									  n_state=s_fbtw;
-									
-								 end
-					 
-				        end
-				 
-						 if(htrans==SEQ || htrans == BUSY)
-						 n_state=c_state;
-						 
-						 if(htrans==IDLE)
-						 n_state=s_idle;		 
-				  
-				 end
-		endtask
-		  
-	   // function to identify address for next beat in a burst
-		function[31:0] nextAddress;
-		     input[2:0] hsize;
-			  input[2:0] hburst;
-			  input[31:0] addr;
-			  input[1:0] htrans;
-			  reg[12:0] burstBytes;//local variables
-			  reg[31:0] wrapBoundary;
-			  reg[31:0] wrapStart;
+										else begin
+											driveEnable='h85;
+											if(iHTRANS==SEQ) begin
+												incBC=1;
+												n_state=s_fbtw;
+										
+											end
+											else
+											n_state=s_fbtw;									
+										
+										end
 						
-					begin
-					   nextAddress=addr;
-						if(htrans!=BUSY) begin
-								if(hburst==INCR || hburst==INCR4 || hburst==INCR8 || hburst==INCR16)
-									nextAddress=addr+2**hsize;									 
-									 
-									 
-								 else begin
-										case(hburst)
-												WRAP4:burstBytes=4*(2**hsize);
-												WRAP8:burstBytes=8*(2**hsize);
-												WRAP16:burstBytes=16*(2**hsize);
-												default: burstBytes=4;									
-										endcase
-										
-										wrapStart=addr-addr%burstBytes;
-										wrapBoundary=wrapStart+burstBytes;
-										
-										if((addr+2**hsize)>=wrapBoundary)
-											nextAddress=wrapStart;
-										else 
-											nextAddress=addr+2**hsize;
+						
+								  end
+								  
+								  else begin
+										if(HRESP=='b0) begin
+										   if(HBURST!=BUSY) begin
+												driveEnable='h00;
+												n_state=s_fbtw;
+											end
 											
-								 end
-						 end
+											else begin
+												driveEnable='h84;
+												n_state=s_fbtw;
+											end
+											
+										end
+										
+										else begin
+											error=1;
+											n_state=s_idle;
+										end
+								  end
 						
-					end
-		endfunction
-		  
+						
+						
+						s_ubtr: begin
+										if(HREADY) begin
+											driveEnable='hfe;
+											sampleData=1;
+											nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
+										end
+										
+										else begin
+											if(HRESP) begin
+												error=1;
+												n_state=s_idle;
+											end
+											
+											else begin
+												if(HBURST==BUSY) begin
+													driveEnable='hfe;
+													nextStateDecider(iHTRANS,iHBURST,iHWRITE,n_state,resetBC,set3,set7,set15);
+												end
+												
+												else begin
+													driveEnable='h00;
+													n_state=s_ubtr;
+												end
+												
+											end
+										end
+								  end
+				
+						
+						default: begin 
+										error=1;
+										n_state=s_idle;
+									end
+				 endcase
+			  end
+			  
+			  
+			 
+		/////////task which determines the next state//////////////////
+			task nextStateDecider;
+				  input[1:0] htrans;
+				  input[2:0] hburst;
+				  input hwrite;
+				  output [2:0] n_state; 
+				  output  resetBC;
+				  output set3;
+				  output set7;
+				  output set15;
+					 begin
+							 n_state=s_idle;
+							 resetBC=1;
+							 set3=0;set7=0;set15=0;
+							 if(htrans==NONSEQ) begin		
+									
+									if(hburst==INCR) 
+										if(hwrite=='b1)
+										n_state=s_ubtw;
+										else
+										n_state=s_ubtr;						
+											
+									 else begin 
+										resetBC=0;
+										case(hburst)
+											INCR4: set3=1;
+											INCR8: set7=1;
+											INCR16: set15=1;
+											WRAP4: set3=1;
+											WRAP8: set7=1;
+											WRAP16: set15=1;
+											default: set3=1;
+										endcase
+										if(hwrite=='b1)
+										  n_state=s_fbtw;
+										else
+										  n_state=s_fbtr;
+										
+									 end
+						 
+							  end
+					 
+							 if(htrans==SEQ || htrans == BUSY)
+							 n_state=c_state;
+							 
+							 if(htrans==IDLE)
+							 n_state=s_idle;		 
+					  
+					 end
+			endtask
+			  
+			// function to identify address for next beat in a burst
+			function[31:0] nextAddress;
+				  input[2:0] hsize;
+				  input[2:0] hburst;
+				  input[31:0] addr;
+				  input[1:0] htrans;
+				  reg[12:0] burstBytes;//local variables
+				  reg[31:0] wrapBoundary;
+				  reg[31:0] wrapStart;
+							
+						begin
+							nextAddress=addr;
+							if(htrans!=BUSY) begin
+									if(hburst==INCR || hburst==INCR4 || hburst==INCR8 || hburst==INCR16)
+										nextAddress=addr+2**hsize;									 
+										 
+										 
+									 else begin
+											case(hburst)
+													WRAP4:burstBytes=4*(2**hsize);
+													WRAP8:burstBytes=8*(2**hsize);
+													WRAP16:burstBytes=16*(2**hsize);
+													default: burstBytes=4;									
+											endcase
+											
+											wrapStart=addr-addr%burstBytes;
+											wrapBoundary=wrapStart+burstBytes;
+											
+											if((addr+2**hsize)>=wrapBoundary)
+												nextAddress=wrapStart;
+											else 
+												nextAddress=addr+2**hsize;
+												
+									 end
+							 end
+							
+						end
+			endfunction
+			  
 
 endmodule
